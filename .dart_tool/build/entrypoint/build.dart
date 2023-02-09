683578d4bab3887b3844749238ff1ab3d03586b9
Vasiliy Borovoy <>
2023-02-09 17:37:27 +0300

    update

---
+++ .dart_tool/build/entrypoint/build.dart
@@ -0,0 +1,83 @@
+// ignore_for_file: directives_ordering
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:build_runner_core/build_runner_core.dart' as _i1;
+import 'package:built_value_generator/builder.dart' as _i2;
+import 'package:freezed/builder.dart' as _i3;
+import 'package:injectable_generator/builder.dart' as _i4;
+import 'package:json_serializable/builder.dart' as _i5;
+import 'package:mockito/src/builder.dart' as _i6;
+import 'package:retrofit_generator/retrofit_generator.dart' as _i7;
+import 'package:source_gen/builder.dart' as _i8;
+import 'dart:isolate' as _i9;
+import 'package:build_runner/build_runner.dart' as _i10;
+import 'dart:io' as _i11;
+
+final _builders = <_i1.BuilderApplication>[
+  _i1.apply(
+    r'built_value_generator:built_value',
+    [_i2.builtValue],
+    _i1.toDependentsOf(r'built_value_generator'),
+    hideOutput: true,
+    appliesBuilders: const [r'source_gen:combining_builder'],
+  ),
+  _i1.apply(
+    r'freezed:freezed',
+    [_i3.freezed],
+    _i1.toDependentsOf(r'freezed'),
+    hideOutput: false,
+  ),
+  _i1.apply(
+    r'injectable_generator:injectable_builder',
+    [_i4.injectableBuilder],
+    _i1.toDependentsOf(r'injectable_generator'),
+    hideOutput: true,
+  ),
+  _i1.apply(
+    r'injectable_generator:injectable_config_builder',
+    [_i4.injectableConfigBuilder],
+    _i1.toDependentsOf(r'injectable_generator'),
+    hideOutput: false,
+  ),
+  _i1.apply(
+    r'json_serializable:json_serializable',
+    [_i5.jsonSerializable],
+    _i1.toDependentsOf(r'json_serializable'),
+    hideOutput: true,
+    appliesBuilders: const [r'source_gen:combining_builder'],
+  ),
+  _i1.apply(
+    r'mockito:mockBuilder',
+    [_i6.buildMocks],
+    _i1.toDependentsOf(r'mockito'),
+    hideOutput: false,
+  ),
+  _i1.apply(
+    r'retrofit_generator:retrofit',
+    [_i7.retrofitBuilder],
+    _i1.toDependentsOf(r'retrofit_generator'),
+    hideOutput: true,
+    appliesBuilders: const [r'source_gen:combining_builder'],
+  ),
+  _i1.apply(
+    r'source_gen:combining_builder',
+    [_i8.combiningBuilder],
+    _i1.toNoneByDefault(),
+    hideOutput: false,
+    appliesBuilders: const [r'source_gen:part_cleanup'],
+  ),
+  _i1.applyPostProcess(
+    r'source_gen:part_cleanup',
+    _i8.partCleanup,
+  ),
+];
+void main(
+  List<String> args, [
+  _i9.SendPort? sendPort,
+]) async {
+  var result = await _i10.run(
+    args,
+    _builders,
+  );
+  sendPort?.send(result);
+  _i11.exitCode = result;
+}
